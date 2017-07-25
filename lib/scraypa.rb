5a720610ece444a6fc0c1bbc10126b97650218ca
joshweir <joshua.weir@gmail.com>
2017-07-25 15:44:50 +1000

    commence config options dev

--- lib/scraypa.rb
+++ lib/scraypa.rb
@@ -1,5 +1,33 @@
 require "scraypa/version"
+require "scraypa/configuration"
+require "scraypa/visit"
+require "scraypa/response"
 
 module Scraypa
-  # Your code goes here...
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.reset
+    @configuration = Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
+  #TODO: configuration includes:
+  # javascript_enabled - means capybara will be used and capybara can be used after visit
+  # by default will use rest client
+  # tor: {torBasePort, torControlPort} - tor will be used for the current process
+  # disguise: {disguise options here}
+  #TODO: possible to change configuration half way through process?
+  #TODO: the response object needs to include the restclient response for rest client
+  #for capybara need to ensure that the page object and basic capybara page response
+  #and interaction stuff works here
+
 end
---
+++ lib/scraypa/configuration.rb
@@ -0,0 +1,10 @@
+module Scraypa
+  class Configuration
+    attr_accessor :option_1, :option_2
+
+    def initialize
+      @option_1 = nil
+      @option_2 = nil
+    end
+  end
+end
