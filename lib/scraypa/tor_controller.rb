8d79aa716fcfb5c59c161bd3240a72b5e1f599ec
joshweir <joshua.weir@gmail.com>
2017-08-03 14:44:29 +1000

    updated tor_controller.rb and tor_controller_spec.rb

--- lib/scraypa/tor_controller.rb
+++ lib/scraypa/tor_controller.rb
@@ -1,5 +1,4 @@
 require 'socksify'
-#require 'net/telnet'
 require 'terminator'
 require 'tor'
 
@@ -13,10 +12,9 @@ module Scraypa
       @settings = {}
       @settings[:tor_port] = params.fetch(:tor_port, 9050)
       @settings[:control_port] = params.fetch(:control_port, 50500)
-      TCPSocket::socks_server = "127.0.0.1"
-      TCPSocket::socks_port = @settings[:tor_port]
       @ip = nil
       @endpoint_change_attempts = 5
+      enable_socks_server
     end
 
     def get_ip
@@ -61,31 +59,23 @@ module Scraypa
     end
 
     def tor_switch_endpoint
-      #Tor::Controller.connect(:port => @settings[:control_port]) do |tor|
-      #  tor.authenticate("password")
-      #  tor.signal("newnym")
-      #  sleep 10
-      #end
-      telnet_pid = nil
-      begin
-        Terminator.terminate :seconds => 20 do
-          #cmd = "bundle exec ruby -e \"require 'net/telnet'\" " +
-          #      " -e \"@tor_telnet = Net::Telnet::new('Host' => '127.0.0.1', 'Port' => '#{@settings[:control_port]}',
-          #        'Timeout' => 10, 'Prompt' => /250 OK\n/)\" " +
-          #      " -e \"@tor_telnet.cmd('AUTHENTICATE """"') { |c| print c; throw 'Cannot authenticate to Tor' if c != '250 OK\n' }\"" +
-          #      " -e \"@tor_telnet.cmd('signal NEWNYM') { |c| print c; throw 'Cannot switch Tor to new route' if c != '250 OK\n' }\"" +
-          #      " -e \"@tor_telnet.close\""
-          #puts "running: #{cmd}"
-          cmd = "bundle exec ruby -e \"require 'tor'\" -e " +
-              "\"Tor::Controller.connect(:port => #{@settings[:control_port]})" +
-              "{|tor| tor.authenticate(''); tor.signal('newnym')}\""
-          telnet_pid = Process.spawn(cmd)
-          Process.wait telnet_pid
-        end
-      rescue Terminator.error
-        puts 'Telnet process to switch Tor endpoint timed out!'
-        ProcessHelper.kill_process(telnet_pid) if telnet_pid
+      disable_socks_server
+      Tor::Controller.connect(:port => @settings[:control_port]) do |tor|
+        tor.authenticate("")
+        tor.signal("newnym")
+        sleep 10
       end
+      enable_socks_server
+    end
+
+    def enable_socks_server
+      TCPSocket::socks_server = "127.0.0.1"
+      TCPSocket::socks_port = @settings[:tor_port]
+    end
+
+    def disable_socks_server
+      TCPSocket::socks_server = nil
+      TCPSocket::socks_port = nil
     end
   end
 end
