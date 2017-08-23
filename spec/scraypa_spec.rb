d44bc2412cae6b707244b6109047bc1bcc3c14ae
joshweir <joshua.weir@gmail.com>
2017-08-23 12:21:03 +1000

    enable user agents (in prog)

--- spec/scraypa_spec.rb
+++ spec/scraypa_spec.rb
@@ -182,83 +182,41 @@ RSpec.describe Scraypa do
             Scraypa.visit method: :get, url: "http://example.com"
           end
         end
-      end
-    end
-  end
 
-=begin
-  describe "puffing billy poltergeist exploratory test", type: :feature,
-           driver: :poltergeist_billy do
-    before :all do
-      proxy.stub('http://www.google.com/')
-          .and_return(:text => "test response")
-    end
-    it 'stubs google.com' do
-      visit "http://www.google.com/"
-      expect(page).to have_content('test response')
-    end
-  end
-=end
+        context "puffing billy poltergeist exploratory test", type: :feature,
+                 driver: :poltergeist_billy do
+          before :all do
+            proxy.stub('http://www.google.com/')
+                .and_return(:text => "test response")
+          end
 
-=begin
-  describe "#visit" do
-    context "when using Rest-Client (not using javascript)" do
-      before :all do
-        Scraypa.reset
-        stub_request(:get, "http://my.mock.site/page.html").
-            with(headers: {'Accept'=>'*/*',
-                           'Accept-Encoding'=>'gzip, deflate',
-                           'Host'=>'my.mock.site',
-                           'User-Agent'=>'rest-client/2.0.2 (linux-gnu x86_64) ruby/2.3.1p112'}).
-            to_return(status: 200, body: "test response", headers: {})
-        @response = Scraypa.visit(:method => :get,
-                                  :url => "http://my.mock.site/page.html",
-                                  :timeout => 3, :open_timeout => 3)
-      end
-      it "utilises rest client to download web content" do
-        expect(@response.class).to eq(RestClient::Response)
-        expect(@response.to_str).to eq('test response')
-      end
-      it_behaves_like 'a Tor-able web agent'
-    end
-    context "when using Capybara (using javascript)" do
-      describe "with headless_chromium driver" do
-        it_behaves_like 'a javascript-enabled web agent (using Capybara)',
-                        driver: :headless_chromium
-        #it_behaves_like 'a Tor-able web agent',
-        #                use_capybara: true,
-        #                driver: :headless_chromium
-        it "does not support Tor" do
-          expect {
-            Scraypa.configure do |config|
-              config.tor = true
-              config.tor_options = {
-                  tor_port: 9055,
-                  control_port: 50500
-              }
-              config.use_capybara = true
-              config.driver = :headless_chromium
-              config.driver_options = {
-                  browser: :chrome,
-                  desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-                      "chromeOptions" => {
-                          'binary' => "#{ENV['HOME']}/chromium/src/out/Default/chrome",
-                          'args' => ["no-sandbox", "disable-gpu", "headless"]
-                      }
-                  )
-              }
-            end
-          }.to raise_error /headless_chromium does not support Tor/
+          it 'stubs google.com' do
+            page.driver.add_headers("User-Agent" => "the user agent string you want")
+            visit "http://www.google.com/"
+            expect(page).to have_content('test response')
+            page.execute_script(
+                "document.getElementsByTagName('body')[0].innerHTML = navigator.userAgent;")
+            expect(page).to have_content('the user agent string you want')
+          end
+        end
+
+        context "puffing billy chrome exploratory test", type: :feature,
+                driver: :selenium_chrome_billy do
+          before :all do
+            proxy.stub('http://www.google.com/')
+                .and_return(:text => "test response")
+          end
+
+          it 'stubs google.com' do
+            page.driver.header("User-Agent" => "the user agent string you want")
+            visit "http://www.google.com/"
+            expect(page).to have_content('test response')
+            page.execute_script(
+                "document.getElementsByTagName('body')[0].innerHTML = navigator.userAgent;")
+            expect(page).to have_content('the user agent string you want')
+          end
         end
       end
-      describe "with poltergeist driver" do
-        it_behaves_like 'a javascript-enabled web agent (using Capybara)',
-                        driver: :poltergeist
-        it_behaves_like 'a Tor-able web agent',
-                        use_capybara: true,
-                        driver: :poltergeist
-      end
     end
   end
-=end
 end
